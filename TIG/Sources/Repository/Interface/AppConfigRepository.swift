581019d089beb64845c5bcf85ffa2606a04b3274
shinseungjae <mok05247@naver.com>
2025-02-04 13:18:30 +0900

    [#3] 줄바꿈 수정

--- TIG/Sources/Repository/Interface/AppConfigRepository.swift
+++ TIG/Sources/Repository/Interface/AppConfigRepository.swift
@@ -16,19 +16,20 @@ protocol AppConfigRepository {
   /// - Returns: 온보딩 완료 여부
   func fetchOnboardingStatus() -> Result<Bool, Error>
   
-  /// 사용자의 기상 시간을 업데이트합니다.
+  /// 기상 시간을 업데이트합니다.
   /// - Parameter time: 설정할 기상 시간 (예: 7 → 오전 7시)
   func updateWakeupTime(_ time: Int)
   
   /// 저장된 기상 시간을 가져옵니다.
-  /// - Returns: 저장된 기상 시간 또는 오류
+  /// - Returns: 저장된 기상 시간
   func fetchWakeupTime() -> Result<Int, Error>
   
-  /// 사용자의 취침 시간을 업데이트합니다.
+  /// 취침 시간을 업데이트합니다.
   /// - Parameter time: 설정할 취침 시간 (예: 22 → 오후 10시)
   func updateBedTime(_ time: Int)
   
   /// 저장된 취침 시간을 가져옵니다.
-  /// - Returns: 저장된 취침 시간 또는 오류
+  /// - Returns: 저장된 취침 시간
   func fetchBedTime() -> Result<Int, Error>
+  
 }
