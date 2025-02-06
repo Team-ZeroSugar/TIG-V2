ae47e414dd907696e867a91df5277696e2fb4805
Jeong Dong <eejxdong99@gmail.com>
2025-02-06 15:51:33 +0900

    [#2] 오늘로 돌아가기 구현

--- TIG/Sources/Feature/Home/HomeViewModel.swift
+++ TIG/Sources/Feature/Home/HomeViewModel.swift
@@ -35,13 +35,14 @@ final class HomeViewModel {
     switch action {
     case .onAppear:
       if state.weekSlider.isEmpty {
-        initializeWeekSlider()
+        state.weekSlider = generateWeekSlider()
       }
     case .moveWeekPeriod(let index):
       if state.weekSlider.indices.contains(index) {
         paginateWeek(currentIndex: index)
       }
     case .selectDate(let date):
+      state.weekSlider = generateWeekSlider(anchor: date)
       state.currentDate = date.formattedDate
     case .tabChange(let tab):
       state.selectedTab = tab
@@ -51,21 +52,28 @@ final class HomeViewModel {
 
 // MARK: - Function
 extension HomeViewModel {
-  /// 오늘 날짜 기준 이전 주, 이번 주, 다음 주 데이터로 초기화
-  private func initializeWeekSlider() {
-    let currentWeek = Date().weekOfDate
+  /// 기준 날짜가 속한 주를 포함한 이전, 다음 주 데이터 묶음을 생성
+  /// - Parameter date: 기준이 되는 날짜
+  /// - Returns: 기준 날짜가 속한 주를 포함한 이전, 다음 주 데이터 묶음
+  private func generateWeekSlider(anchor date: Date = .now) -> [[Date.WeekDay]] {
+    var newWeeks = [[Date.WeekDay]]()
+    let anchorWeek = date.weekOfDate
     
-    if let firstDate = currentWeek.first?.date {
-      state.weekSlider.append(firstDate.createPreviousWeek())
+    if let firstDate = anchorWeek.first?.date {
+      newWeeks.append(firstDate.createPreviousWeek())
     }
     
-    state.weekSlider.append(currentWeek)
+    newWeeks.append(anchorWeek)
     
-    if let lastDate = currentWeek.last?.date {
-      state.weekSlider.append(lastDate.createNextWeek())
+    if let lastDate = anchorWeek.last?.date {
+      newWeeks.append(lastDate.createNextWeek())
     }
+    
+    return newWeeks
   }
   
+  /// 현재 주간 캘린더 위치가 weekSlider의 첫번째 또는 마지막인 경우 새로 생성
+  /// - Parameter currentIndex: 현재 위치한 주간 캘린더 인덱스
   private func paginateWeek(currentIndex: Int) {
     if let firstDate = state.weekSlider[currentIndex].first?.date,
        currentIndex == 0 {
